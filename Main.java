e4ec7bfbfe26a3f5efccb9a2064e5dc967814c4b
fiscn <fiscn2709@wrdsb.ca>
2020-10-01 19:36:25 +0000

    Finished A3 Q4 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ *  a program that will ask the user for a positive integer. If that number is between 1 and 10 inclusively, the program will print out the “written form” of the number. If it is not within that range, the computer prints out “not in the range”.
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,47 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create Scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //Prompt the user to insert an integer 
+    System.out.println("Please enter a positive integer:"); 
+    int Positive = input.nextInt(); 
+
+    switch(Positive){
+    case 1: 
+    System.out.println("One"); 
+    break; 
+    case 2: 
+    System.out.println("Two"); 
+    break;
+    case 3: 
+    System.out.println("Three"); 
+    break;
+    case 4: 
+    System.out.println("Four"); 
+    break;
+    case 5: 
+    System.out.println("Five"); 
+    break;
+    case 6: 
+    System.out.println("Six"); 
+    break; 
+    case 7:
+    System.out.println("Seven"); 
+    break; 
+    case 8: 
+    System.out.println("Eight"); 
+    break;
+    case 9: 
+    System.out.println("Nine"); 
+    break; 
+    case 10:
+    System.out.println("Ten"); 
+    break; 
+    default: 
+    System.out.println("Not in range");
+    }
     
   }
 }
